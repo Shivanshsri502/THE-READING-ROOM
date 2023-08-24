351a1920c96a73444a3f18c5273607c1439d9b6f
Shivanshsri502 <shivanshsri502@gmail.com>
2023-08-25 02:10:51 +0530

    j

--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -11,7 +11,7 @@ const SearchInput = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        `https://zsxs.onrender.com//api/v1/product/search/${values.keyword}`
+        `https://zsxs.onrender.com/api/v1/product/search/${values.keyword}`
       );
       setValues({ ...values, results: data });
       navigate("/search");
